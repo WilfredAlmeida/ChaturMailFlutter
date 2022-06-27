62fe987d48dd9870ce36a44777b18c5eb65274f6
almei <almeidawilfred642@gmail.com>
2022-06-27 14:50:01 +0530

    Notofications Done

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:wilfredemail/controllers/storage_controller.dart';
 import 'package:wilfredemail/controllers/user_controller.dart';
 import 'package:wilfredemail/models/tutorials_model.dart';
 import 'package:wilfredemail/view_models/tutorials_viewmodel.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import './models/past_emails_model.dart';
 import './utils/utils_controller.dart';
 import './view_models/prompt_viewmodel.dart';
@@ -26,11 +28,41 @@ import 'views/screens/dashboard.dart';
 import 'views/widgets/tutorial_detail_widget.dart';
 import 'views/screens/tutorial_screen.dart';
 
-void main() async {
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    importance: Importance.high,
+    playSound: true);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('A bg message just showed up :  ${message.messageId}');
+}
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+
 
   await Hive.initFlutter();
 
@@ -86,6 +118,50 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     userLoggedInFuture = isUserLoggedIn();
+
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification!;
+      AndroidNotification android = message.notification!.android!;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                color: Colors.blue,
+                playSound: true,
+                icon: '@mipmap/ic_launcher.png',
+              ),
+            ));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      RemoteNotification notification = message.notification!;
+      AndroidNotification android = message.notification!.android!;
+      if (notification != null && android != null) {
+        // showDialog(
+        //     context: context,
+        //     builder: (_) {
+        //       return AlertDialog(
+        //         title: Text(notification.title!),
+        //         content: SingleChildScrollView(
+        //           child: Column(
+        //             crossAxisAlignment: CrossAxisAlignment.start,
+        //             children: [Text(notification.body!)],
+        //           ),
+        //         ),
+        //       );
+        //     });
+      }
+    });
+
+
 
     super.initState();
   }
